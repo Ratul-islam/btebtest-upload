56681901cb94abe6f5212e34e180b7c1349d6035
ratul-islam <ratulislam20502020@gmail.com>
2023-05-26 21:46:26 +0600

    messed up on the server page

--- src/app/getpdf/page.jsx
+++ src/app/getpdf/page.jsx
@@ -1,22 +1,55 @@
 import fs from 'fs';
+import path from 'path';
 import pdf from 'pdf-parse'
+import LoadData from '@/component/loadpdf/page';
+import { get } from 'http';
+import { edgeServerAppPaths } from 'next/dist/build/webpack/plugins/pages-manifest-plugin';
 
-import LoadPdf from '@/component/loadpdf/page';
 
 
 
-  const  GetData =async ()=> {
-    
-  let dataBuffer = fs.readFileSync('./test/data/result_tour_6th.pdf');
+  const  GetData  = async ()=> {
 
-   
+    var allLocation = []
+  
+    const getAll =async () =>{
+      "use server"
+      fs.readdir('./test/data',function(err, data){
+        if(err){
+          console.log(err)
+          return;
+        }
+        var gg = data.text
+        // return JSON.stringify(data)
+        // data.map((file)=>{
+        //   let dataBuffer = fs.readFileSync('./test/data/'+file);
+        //   pdf(dataBuffer).then(function(data) { 
+        //     return <LoadData data={data.text}/>
+        // });
+        // })
+        console.log(gg)
+      })
+    }
+    // getAll(another)
+    // console.log('=--------------=')
+    // console.log(getAll())
+    getAll()
   return (
     <div className="container">
       {  
-      pdf(dataBuffer).then(function(data) {
-        console.log(data.metadata)
-           return <LoadPdf data = {JSON.stringify(data.text)} />
-    })}
+      fs.readdir('./test/data',function(err, data){
+        // if(err){
+        //   console.log(err)
+        //   return;
+        // }
+        data.map((file)=>{
+          let dataBuffer = fs.readFileSync('./test/data/'+file);
+          return pdf(dataBuffer).then(function(data) { 
+            return <LoadData data={data.text}/>
+        });
+        })
+      })
+      }
     </div>
   );
 }
--- src/component/loadpdf/page.jsx
+++ src/component/loadpdf/page.jsx
@@ -4,54 +4,88 @@ import React, {useState} from 'react'
 
 import styles from './styles.module.css'
 
-const GetData = ({data}) => {
-  // const [rolls, setRolls] = useState()
-  var rawData = data
-  var rolls = data.match(/\d{6}/g);
-  const [finalData, setFinalData] = useState([])
+const LoadData = ({data}) => {
+    console.log(data)
+    const [finalData, setFinalData] = useState([])
+    var rawData = data;
+    var collages = data?.match(/\d{5}/g);
+    var rolls
+    const clgArr=[];
+    const endLocation=[];
+    const newData=[];
 
-
+    for(var b = 0; b<= collages?.length;b=b+1){
+        if(rawData[rawData.indexOf(collages[b])+6] == '-'){
+            for(var e = rawData.indexOf(collages[b])+6; e<=rawData.length; e= e+1){
+                if(rawData[e] == ','){
+                    clgArr.push(rawData.slice(rawData.indexOf(collages[b])+7,e-1))
+                    break
+                }
+            }
+        }
+    }
+    var notes = Array.from(rawData?.matchAll("Note:"))
+    notes.map((note)=> endLocation.push(note.index))
+    for(let times= 0; times<clgArr.length; times++){
+        let trimedData = {
+            college: clgArr[times],
+            data: rawData.slice(rawData.indexOf(clgArr[times]),endLocation[times])
+        }
+        newData.push(trimedData)
+    }
 
   const handleClick = () =>{
-
-
-
-    for(var i=0;i<=rolls.length;i++){
-      if(rawData[rawData.indexOf(rolls[i])+7]==`(`){
-         
-         let info = {
-             roll: rolls[i],
-             result: {
-              semesterName : rawData.slice(rawData.indexOf(rolls[i])+8 , rawData.indexOf(rolls[i])+12)
-          },
-             passed: true,
-         }
-         setFinalData((oldData) => [...oldData , {info}])
     
-     }   
+    newData.map((data)=>{
+        rolls = data.data.match(/\d{6}/g);
+        for(var i=0;i<=rolls.length;i++){
+            if(rawData[rawData.indexOf(rolls[i])+7]==`(`){
+                
+                let info = {
+                    roll: rolls[i],
+                    technology: rawData.slice(rawData.match('Examination of').index +14,rawData.match('held in').index-7),
+                    regulation : rawData.slice(rawData.match('Regulation').index-5 ,rawData.match('Regulation').index),
+                    college: data.college,
+                    result: [{
+                        semester: rawData.slice(rawData.match('th Semester').index-2 ,rawData.match('th Semester').index+3),
+                        status : rawData.slice(rawData.indexOf(rolls[i])+8 , rawData.indexOf(rolls[i])+12)
+                    }],
+                    passed: true,
+                }
+                setFinalData((oldData) => [...oldData , {info}])
+            
+            }   
      }
-     
- 
-     for(var i=0;i<=rolls.length;i++){
-         if(rawData[rawData.indexOf(rolls[i])+7]==`{`){
-             for(var start = rawData.indexOf(rolls[i])+7; start<=rawData.length; start = start+1 ){
-                 if(rawData[start]=='}'){
- 
-                     let info = {
-                         roll: rolls[i],
-                         result: {semesterName: rawData.slice(rawData.indexOf(rolls[i])+7,start+1)},
-                         passed: false,
-                     }
-                     setFinalData((oldData) => [...oldData , {info}])
-                     break
-                 }
-             }
- 
-         }
-     }
-}
-console.log(finalData)
+        for(var i=0;i<=rolls.length;i++){
+                if(rawData[rawData.indexOf(rolls[i])+7]==`{`){
+                    for(var start = rawData.indexOf(rolls[i])+7; start<=rawData.length; start = start+1 ){
+                        if(rawData[start]=='}'){
+        
+                            let info = {
+                                roll: rolls[i],
+                                technology: rawData.slice(rawData.match('Examination of').index +14,rawData.match('held in').index-7),
+                                regulation : rawData.slice(rawData.match('Regulation').index-5 ,rawData.match('Regulation').index),
+                                college: data.college,
+                                result: [{
+                                    semester: rawData.slice(rawData.match('th Semester').index-2 ,rawData.match('th Semester').index+3),
+                                    status : rawData.slice(rawData.indexOf(rolls[i])+8 , rawData.indexOf(rolls[i])+12)
+                                }],
+                                passed: false,
+                            }
+                            setFinalData((oldData) => [...oldData , {info}])
+                            break
+                        }
+                    }
+        
+                }
+            }
 
+
+
+    })
+}
+
+console.log(finalData)
     return(
       <div>
         <button onClick={handleClick}>click me!</button>
@@ -59,4 +93,4 @@ console.log(finalData)
     )
 }
 
-export default GetData;
+export default LoadData;
